68778f235f108079ca3f744cadc7186bf71037d6
Helder-Rodrigues <hmrodrigues4@gmail.com>
2024-05-20 17:59:13 +0100

    Added Stars

--- Assets/Scripts/Attributes.cs
+++ Assets/Scripts/Attributes.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public static class Attributes
 {
+    private static int currLvl = 0;
+    public static int CurrLvl
+    {
+        set { if (value >= 0 && value <= 2) { currLvl = value; } }
+        get { return currLvl; }
+    }
+
     public static int points = 0;
 
     public static int speed = 5;
--- Assets/Scripts/Score_Timer.cs
+++ Assets/Scripts/Score_Timer.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Score_Timer : MonoBehaviour
 {
+    [SerializeField] private Texture emptyStarTex;
+    [SerializeField] private Texture filledStarTex;
+
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private TextMeshProUGUI resultText;
 
@@ -10,6 +14,10 @@ public class Score_Timer : MonoBehaviour
 
     private float timer;
     private bool isTimerRunning;
+
+    private readonly int[] fastTimeMin = { 3, 6, 9 };
+    private readonly int[] mediumTimeMin = { 6, 10, 15 };
+    private readonly int[] slowTimeMin = { 10, 15, 25 };
 
     private void Start()
     {
@@ -39,17 +47,30 @@ public class Score_Timer : MonoBehaviour
 
     private void DetermineResult()
     {
-        if (timer <= 3f)
-            Attributes.points += 1;
-        else if (timer > 3f && timer < 7f)
-            Attributes.points += 3;
-        else
-            Attributes.points += 0;
+        int stars = 0;
+        if (timer <= fastTimeMin[Attributes.CurrLvl])
+            stars = 3;
+        else if (timer <= mediumTimeMin[Attributes.CurrLvl])
+            stars = 2;
+        else if (timer <= slowTimeMin[Attributes.CurrLvl])
+            stars = 1;
+
+        Attributes.points += stars;
+        AddStars(stars);
 
         shop.UpdatePointsTxt();
     }
 
-    void UpdateTimerText()
+    private void AddStars(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+            GameObject.Find("star" + (i + 1)).GetComponent<RawImage>().texture = filledStarTex;
+        
+        for (int i = amount; i < 3; i++)
+            GameObject.Find("star" + (i + 1)).GetComponent<RawImage>().texture = emptyStarTex;
+    }
+
+    private void UpdateTimerText()
     {
         int minutes = Mathf.FloorToInt(timer / 60);
         float seconds = timer % 60;
